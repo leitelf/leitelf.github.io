b5dc47929b755099b57805080f01286fab824074
Luiz Leite <feitosaleitelf@gmail.com>
2018-01-22 09:28:54 -0800

    ()

--- js/language.js
+++ js/language.js
@@ -3,8 +3,8 @@ var language = (navigator.browserLanguage != undefined)? navigator.browserLangua
 language = language.substring(0, 2);
 pathname = window.location.pathname;
 
-if (language != "pt") && (pathname == '') {
+if ((language != "pt") && (pathname == '')) {
     window.location.href = window.location.hostname + '/en';
-} else if (language == "pt")  && (pathname != ''){
+} else if ((language == "pt")  && (pathname != '')){
     window.location.href = window.location.hostname;
 }
